f5a45fcf298ad9879768e3e16156d24d6fdcdecf
Ryan Siu <rsiu@ucsb.edu>
2021-06-02 19:14:17 -0700

    creating cust and acc tables

--- db.sql
+++ db.sql
@@ -15,5 +15,21 @@ DROP TABLE IF EXISTS movie_contract;
 SET foreign_key_checks = 1;
 
 CREATE TABLE Customer(
-    
+    tax_id int(9) UNIQUE, 
+	username varchar(255) PRIMARY KEY, 
+    password varchar(255),
+	name varchar(255), 
+	state char(2),
+	phonenumber varchar(100),
+	email varchar(255)
+);
+
+CREATE TABLE Accounts(
+    id_num int(100) AUTO_INCREMENT,
+    own varchar(255) NOT NULL,
+	PRIMARY KEY(id_num, own),
+	FOREIGN KEY(own)
+		REFERENCES Customer(username)
+		ON UPDATE CASCADE ON DELETE CASCADE
+	);
 )
